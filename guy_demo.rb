2057ad5883a1c904a672df16c5adc79f5ae08dc8
SkinnyPigeon <philtheskinnypigeon@yahoo.co.uk>
2016-06-28 22:58:37 +0100

    got the first frame of the ledge animation working but the animation can start again in the wrong position

--- guy_demo.rb
+++ guy_demo.rb
@@ -45,7 +45,7 @@ def win?
 end
 
 def guy_fall(guy1, guy2, guy3, guy4)
-  1..2.times do
+  # 1..2.times do
     guy(guy1)
     animate
     guy(guy2)
@@ -54,26 +54,62 @@ def guy_fall(guy1, guy2, guy3, guy4)
     animate
     guy(guy4)
     animate
-  end
+  # end
 end
 
 
 def ledge
   choose_guy = rand(0..3)
   @replace = @guys[choose_guy]
-  @replace.delete_at(0)
+  @replace.pop
   @replace.concat([['-']])
+  guy(@guys[choose_guy])
+  sleep(0.1)
+  system("clear")
 end
 
-def return_guy
-binding.pry
-nil
+def first_push
+  case 
+
+    when @replace == @guy1
+      @guy1.pop
+      @guy1.concat([['|']])
+      @guy2[3].shift
+      @guy2[3].unshift('-')
+      return
+
+    when @replace == @guy2
+      @guy2.pop
+      @guy2.concat([['|']])
+      @guy3[3].shift
+      @guy3[3].unshift('-')
+      return
+
+    when @replace == @guy3
+      @guy3.pop
+      @guy3.concat([['|']])
+      @guy4[3].shift
+      @guy4[3].unshift('-')
+      return
+
+    when @replace == @guy4
+      @guy4.pop
+      @guy4.concat([['|']])
+      @guy1[3].shift
+      @guy1[3].unshift('-')
+      return
+
+  end
 end
+
+
 
 # binding.pry
 guy_fall(@guy1, @guy2, @guy3, @guy4)
 ledge
-return_guy()
+first_push
+guy_fall(@guy1, @guy2, @guy3, @guy4)
+binding.pry
 nil
 # guy_fall(@guy1, @guy2, @guy3, @guy4)
 # return_guy(@guys)
